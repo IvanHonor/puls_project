ca7f0c8353c5dbcf391a49257a858c8af8dbb435
Ivan Ryzhkov <van-astra.net@list.ru>
2022-07-26 09:52:25 +0300

    add php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,65 @@ $(document).ready(function(){
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
 
+	// Modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn();
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #thanks, #order').fadeOut();
+	})
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		})
+	});
+
+
+	function validForms(form){
+		$(form).validate( {
+			rules: {
+				name: "required",
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Пожалуйста, введите свое имя",
+				phone: "Пожалуйста, введите номер телефона",
+				email: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введен адрес почты"
+				}
+			}
+		});
+	};
+	validForms('#consultation form');
+	validForms('#order form');
+	validForms('#consultation-form');
+	
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function(){
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn();
+			
+			$('form').trigger('reset');
+		});
+		return false;
+
+	});
+
+
 });
