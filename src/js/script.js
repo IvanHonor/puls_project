9edf3387756909b47f91af520fc629c0cce8e170
Ivan Ryzhkov <van-astra.net@list.ru>
2022-07-27 10:43:57 +0300

    before validation

--- src/js/script.js
+++ src/js/script.js
@@ -95,5 +95,23 @@ toggleSlide('.catalog-item__back');
 
 	});
 
+	// Smooth scroll and pageup
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+	new WOW().init();
+
 
 });
